198d2f25da75f32d1b7d470af57d0aebdca73fd4
Derek Meneghel <derekmeneghel2004@gmail.com>
2025-09-27 18:40:07 -0300

    First commit

---
+++ src/main/java/com/example/Derek/workshop_API/WorkshopApiMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.Derek.workshop_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopApiMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApiMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Derek/workshop_API/WorkshopApiMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Derek.workshop_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApiMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
